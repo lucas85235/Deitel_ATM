71de1e9297715101c60139ee4fabe5939277ff30
Lucas de Souza Lima <lzl@certi.org.br>
2019-12-19 15:18:02 -0400

    Create Project Structure

---
+++ build_proj.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo ""
+echo "Verificando a existencia de diretorios . . ."
+echo ""
+
+#if [ -d $(pwd)/build ]
+#then
+#    rm -rf $(pwd)/build
+#fi
+
+echo "Compilando CMake"
+echo ""
+cmake -H. -Bbuild
+echo ""
+
+echo "Compilando MakeFiles"
+echo ""
+cd $(pwd)/build
+make
+echo ""
+
+exit
